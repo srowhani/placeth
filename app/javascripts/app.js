e603ed1a7fe4b3dbf069a3c998d18e4418b78117
Erik Burton <eb.erik.burton@gmail.com>
2018-02-17 15:32:03 -0500

    small refactor, limited colors

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,55 +1,75 @@
-import '../stylesheets/app.css';
+import "../stylesheets/app.css";
 
-import { injectWeb3, injectContract } from './inject';
-import Poller from './poller';
+import { injectWeb3, injectContract } from "./inject";
+import Poller from "./poller";
 
 (async () => {
-    const context = {
-      _lastSyncedBlockNumber: 0
-    };
-    const cHeight = 500,
+  const context = {
+    _lastSyncedBlockNumber: 0
+  };
+  const cHeight = 500,
     cWidth = 500,
-    sWidth = 100,
     size = 5,
     columns = cWidth / size,
     rows = cHeight / size,
     magnifySize = size / 2 - 1 || 1;
 
+  const colors = [
+    { r: 34, g: 34, b: 34 }, //Black
+    { r: 229, g: 0, b: 0 }, //Red
+    { r: 130, g: 0, b: 128 }, //Purple
+    { r: 160, g: 106, b: 66 }, //Brown
+    { r: 136, g: 136, b: 136 }, //Grey
+    { r: 148, g: 224, b: 68 }, //Light Green
+    { r: 207, g: 110, b: 228 }, //Light Purple
+    { r: 228, g: 228, b: 22 }, //Light Grey
+    { r: 2, g: 190, b: 1 }, //Green
+    { r: 0, g: 0, b: 234 }, //Royal Blue
+    { r: 0, g: 131, b: 199 }, //Light Blue
+    { r: 229, g: 149, b: 0 }, //Orange/Gold
+    { r: 0, g: 211, b: 221 }, //Cyan
+    { r: 229, g: 217, b: 0 }, //Yellow
+    { r: 255, g: 167, b: 209 }, //Pink
+    { r: 255, g: 255, b: 255 } //White
+  ];
   let selected = {
     active: false,
     x: -1,
     y: -1
   };
-
   let colorMap, squareX, squareY;
 
-  const web3 = await injectWeb3()
-  const contract = await injectContract(web3.currentProvider)
+  const web3 = await injectWeb3();
+  const contract = await injectContract(web3.currentProvider);
 
-  console.log(contract)
+  console.log(contract);
 
   const poller = Poller.init();
 
-  context.address = web3.eth.accounts[0]
+  context.address = web3.eth.accounts[0];
 
-  poller.queue('sync', () => {
-    context.address = web3.eth.accounts[0]
-  })
+  poller.queue("sync", () => {
+    context.address = web3.eth.accounts[0];
+  });
 
   const _reference = new p5(instance => {
-    poller.queue('render', () => {
-      const _commitEvent = contract.Commit({}, {
-        fromBlock: 1 + context._lastSyncedBlockNumber,
-        toBlock: 'latest'
-      }, (error, result) => {
-        console.log(result)
+    poller.queue("render", () => {
+      const _commitEvent = contract.Commit(
+        {},
+        {
+          fromBlock: 1 + context._lastSyncedBlockNumber,
+          toBlock: "latest"
+        },
+        (error, result) => {
+          console.log(result);
 
-        if (!error) {
-          context._lastSyncedBlockNumber = result.blockNumber
-          requestAnimationFrame(_ => drawToCanvas(result))
+          if (!error) {
+            context._lastSyncedBlockNumber = result.blockNumber;
+            requestAnimationFrame(_ => drawToCanvas(result));
+          }
         }
-      })
-    })
+      );
+    });
 
     // Preload - gets called before setup or draw
     instance.preload = async () => {
@@ -58,26 +78,18 @@ import Poller from './poller';
       for (var x = 0; x < colorMap.length; x++) {
         colorMap[x] = new Array(rows);
         for (var y = 0; y < colorMap[x].length; y++) {
-          colorMap[x][y] = randomColor();
+          colorMap[x][y] = rand(16);
         }
       }
 
       function rand(max) {
         return Math.floor(Math.random() * Math.floor(max));
       }
-
-      function randomColor() {
-        return {
-          r: rand(255),
-          g: rand(255),
-          b: rand(255)
-        };
-      }
     };
 
     //Setup - Creates canvas commences drawing
     instance.setup = () => {
-      const canvas = instance.createCanvas(cWidth + sWidth, cHeight);
+      const canvas = instance.createCanvas(cWidth, cHeight);
       canvas.parent("application");
       instance.frameRate(0);
       instance.draw();
@@ -86,75 +98,75 @@ import Poller from './poller';
     //Draw
     instance.draw = () => {
       drawGrid();
+      drawSelected();
     };
 
+    //Draws grid
     function drawGrid() {
-      let def;
+      instance.strokeWeight(0);
 
       for (let x = 0; x < colorMap.length; x++) {
         for (let y = 0; y < colorMap[x].length; y++) {
-          instance.strokeWeight(0);
           const idx = colorMap[x][y];
-          const { r, g, b } = idx;
+          const { r, g, b } = colors[idx];
           instance.fill(instance.color(r, g, b));
           instance.rect(x * size, y * size, size, size);
         }
       }
-
-      if (selected.active) {
-        const sqr = colorMap[selected.x][selected.y];
-
-        instance.fill(instance.color(sqr.r, sqr.g, sqr.b));
-        instance.rect(
-          selected.x * size - magnifySize,
-          selected.y * size - magnifySize,
-          size + magnifySize*2,
-          size + magnifySize*2
-        );
-      }
     }
 
-    instance.mouseClicked = async e => {
-      const { x, y } = e;
+    function drawSelected() {
+      if (!selected.active) return;
 
-      console.log(instance.mouseX, instance.mouseY);
+      const idx = colorMap[selected.x][selected.y];
+      const { r, g, b } = colors[idx];
 
-      let mouseX = instance.mouseX;
-      let mouseY = instance.mouseY;
-
-      selected.active = true;
-
-      if (x < 0 || y < 0 || mouseX > cWidth + sWidth || mouseY > cHeight) {
-        return;
-      }
-
-      selected.x = Math.floor(mouseX / size);
-      selected.y = Math.floor(mouseY / size);
-
-      context._activeItem = {
-        x: selected.x,
-        y: selected.y
-      };
-
-      console.log(
-        `Click at ${mouseX}, ${mouseY} === ${selected.x}, ${selected.y}`
+      instance.strokeWeight(1);
+      instance.fill(instance.color(r, g, b));
+      instance.rect(
+        selected.x * size - magnifySize,
+        selected.y * size - magnifySize,
+        size + magnifySize * 2,
+        size + magnifySize * 2
       );
+    }
 
-      instance.draw();
+    instance.mouseClicked = async () => {
+      const mouseX = instance.mouseX,
+        mouseY = instance.mouseY;
 
-      Object.assign(context, hexToRgb(context._activeColor));
-      const { r, g, b } = colorMap[selected.x][selected.y];
+      // Square Selection
+      if (mouseX > 0 && mouseY > 0 && mouseX < cWidth && mouseY < cHeight) {
+        selected.active = true;
 
-      try {
-        const tx = await contract.fill(selected.x, selected.y, r, g, b, {
-          from: context.address,
-          to: contract.address,
-          gas: 41000
-        });
-        console.log(tx);
-      } catch (err) {
-        console.trace(err)
+        selected.x = Math.floor(mouseX / size);
+        selected.y = Math.floor(mouseY / size);
+
+        context._activeItem = {
+          x: selected.x,
+          y: selected.y
+        };
+
+        console.log(
+          `Click at ${mouseX}, ${mouseY} === ${selected.x}, ${selected.y}`
+        );
+
+        instance.draw();
       }
+
+      // Object.assign(context, hexToRgb(context._activeColor));
+      // const { r, g, b } = colorMap[selected.x][selected.y];
+
+      // try {
+      //   const tx = await contract.fill(selected.x, selected.y, r, g, b, {
+      //     from: context.address,
+      //     to: contract.address,
+      //     gas: 41000
+      //   });
+      //   console.log(tx);
+      // } catch (err) {
+      //   console.trace(err);
+      // }
     };
   });
   function hexToRgb(hex) {
